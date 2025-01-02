664aff2a8ed2d53220eaec8e8c15b4e1828f270d
Dan Omelyanyuk <bass.chaleng@gmail.com>
2025-01-03 03:49:22 +0700

    Added GET tests

--- CSD-PL-1/tests.py
+++ CSD-PL-1/tests.py
@@ -1,16 +1,44 @@
 import requests
 
+response = requests.get('http://localhost:8000/')
+assert response.status_code == 200
+print(response.text)
+
+response = requests.get('http://localhost:8000/timezones')
+assert response.status_code == 200
+print("All good!")
+
+response = requests.get('http://localhost:8000/Europe/Moscow')
+assert response.status_code == 200
+print(response.text)
+
+response = requests.get('http://localhost:8000/UnknownZone')
+assert response.status_code == 200
+print(response.text)
+
 response = requests.post(
     'http://localhost:8000/api/v1/time',
     json={'tz': 'Europe/Moscow'}
 )
-print(response.json())
+print("time", response.json())
 
 response = requests.post(
     'http://localhost:8000/api/v1/date',
     json={'tz': 'Europe/Paris'}
 )
-print(response.json())
+print("date", response.json())
+
+response = requests.post(
+    'http://localhost:8000/api/v1/time',
+    json={'tz': ''}
+)
+print("time, no tz", response.json())
+
+response = requests.post(
+    'http://localhost:8000/api/v1/date',
+    json={'tz': ''}
+)
+print("date, no tz", response.json())
 
 response = requests.post(
     'http://localhost:8000/api/v1/datediff',
